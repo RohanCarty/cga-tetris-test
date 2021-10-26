16e2057cb128dab2604b7f98bd0fd9601e0b4536
Rohan Carty <rohan253@hotmail.com>
2021-10-26 23:43:41 +1100

    Updated CGA Hello to draw a sprite, will incorporate into main library

--- CGAHELLO.C
+++ CGAHELLO.C
@@ -23,14 +23,24 @@ int main()
 
     setVideoMode(VIDEO_MODE);
 
-    *Video = 0xFFFF;
-    for(i = 0; i < 8192; i++)
+    //*Video = 0x40C8;
+    /*for(i = 0; i < 8192; i++)
     {
 	*Index++;
-	*Index = 0x5656
+	*Index = 0x0280
 
 	;
-    }
+    }*/
+
+    //Draw my test sprite
+    *Index = 0xAAAA;     	    // first even line
+    Index[4096] = 0x0280;       // first odd line
+    Index[40] = 0xAA8A;         // even line
+    Index[4096+40] = 0x2A8A;    // odd line
+    Index[80] = 0x2A88;         // even line
+    Index[4096+80] = 0x828A;    // odd line
+    Index[120] = 0x828A;        // even line
+    Index[4096+120] = 0xAAAA;   // odd line
 
     getchar();
 
